e7cc665dd4c1c86e50c4955006c950cc3b7f2504
Sergey-Kosenko <serg.cos@mail.ru>
2021-05-19 14:17:45 +0500

    Задание 8 выполнено

---
+++ Lesson8/chat_server/src/main/java/ru/kosenko/chat_server/auth/AuthService.java
@@ -0,0 +1,10 @@
+package ru.kosenko.chat_server.auth;
+
+public interface AuthService {
+    void start();
+    void stop();
+    String getUsernameByLoginAndPassword(String login, String password);
+    String changeUsername(String oldName, String newName);
+    String changePassword(String username, String oldPassword, String newPassword);
+
+}
